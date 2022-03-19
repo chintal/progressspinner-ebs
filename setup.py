decf5122df5e4c830d1f49a7d011fe5b84f21da6
Chintalagiri Shashank <shashank.chintalagiri@gmail.com>
2022-03-19 13:01:11 +0530

    Minor tweaks for PyPI

--- setup.py
+++ setup.py
@@ -2,8 +2,8 @@ import os
 
 from setuptools import find_packages, setup
 
-with open(os.path.join(os.path.dirname(__file__), 'README.md')) as readme:
-    README = readme.read()
+# with open(os.path.join(os.path.dirname(__file__), 'README.md')) as readme:
+#     README = readme.read()
 
 # allow setup.py to be run from any path
 os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
@@ -12,14 +12,14 @@ setup(
     name='progressspinner-ebs',
     version='0.1',
     author='Kathryn Taylee',
-    author_email='kived@github',
+    author_email='kived@github.com',
     maintainer='Chintalagiri Shashank',
     maintainer_email='shashank@chintal.in',
     url='https://github.com/chintal/progressspinner-ebs',
     packages=find_packages(),
     include_package_data=True,
     description='Fork of kivy-garden progress spinner',
-    long_description=README,
+    long_description="",
     install_requires=[
         'kivy>=1.11.1',
     ]
