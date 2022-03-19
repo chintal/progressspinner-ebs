d282eddb99a5e02f7f84d1d446c2229b809d47b0
Chintalagiri Shashank <shashank.chintalagiri@gmail.com>
2022-03-19 12:58:12 +0530

    Add note to readme and update setup.py

--- setup.py
+++ setup.py
@@ -9,8 +9,13 @@ with open(os.path.join(os.path.dirname(__file__), 'README.md')) as readme:
 os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
-    name='kivy.garden.progressspinner',
+    name='progressspinner-ebs',
     version='0.1',
+    author='Kathryn Taylee',
+    author_email='kived@github',
+    maintainer='Chintalagiri Shashank',
+    maintainer_email='shashank@chintal.in',
+    url='https://github.com/chintal/progressspinner-ebs',
     packages=find_packages(),
     include_package_data=True,
     description='Fork of kivy-garden progress spinner',
